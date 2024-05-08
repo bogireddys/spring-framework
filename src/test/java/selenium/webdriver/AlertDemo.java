0a9e347397c399b6c0b11390f3a34742e3c80122
Gnanendra Bogireddy <meetbogireddyhere@gmail.com>
2024-05-08 11:42:01 +0530

    Modified AlertDemo.java file

--- src/test/java/selenium/webdriver/AlertDemo.java
+++ src/test/java/selenium/webdriver/AlertDemo.java
@@ -1,7 +1,18 @@
 package selenium.webdriver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class AlertDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setCapability("browserName", "chrome");
+        chromeOptions.addArguments("start-maximized");
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.78.18:4444"), chromeOptions);
 
     }
 }
