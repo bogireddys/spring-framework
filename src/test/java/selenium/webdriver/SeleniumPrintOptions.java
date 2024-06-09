8ff67ddcd81caaeca06d1884fc7145fcf78386e8
Gnanendra Bogireddy <meetbogireddyhere@gmail.com>
2024-06-09 14:14:49 +0530

    Modified pdf generation source code and file

--- src/test/java/selenium/webdriver/SeleniumPrintOptions.java
+++ src/test/java/selenium/webdriver/SeleniumPrintOptions.java
@@ -1,5 +1,6 @@
 package selenium.webdriver;
 
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Pdf;
 import org.openqa.selenium.PrintsPage;
 import org.openqa.selenium.WebDriver;
@@ -11,6 +12,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.Base64;
 
 public class SeleniumPrintOptions {
@@ -21,23 +23,16 @@ public class SeleniumPrintOptions {
         chromeOptions.setCapability("browserName", "chrome");
         chromeOptions.addArguments("start-maximized");
         chromeOptions.addArguments("headless");
-        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.78.18:4444"), chromeOptions);
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
 
         driver.get("https://www.selenium.dev/selenium/web/printPage.html");
 
         PrintOptions printOptions = new PrintOptions();
-        printOptions.setPageRanges("1-4");
-        printOptions.setOrientation(PrintOptions.Orientation.PORTRAIT);
 
         PrintsPage printer = (PrintsPage) driver;
         Pdf pdf = printer.print(printOptions);
         String content = pdf.getContent();
-        byte[] base64String = Base64.getMimeDecoder().decode(content);
         File file = new File("src/test/resources/samplepdf.pdf");
-        FileWriter myWriter = new FileWriter(file);
-        myWriter.write(new String(base64String));
-        myWriter.close();
-        System.out.println("Page Content is : " + new String(base64String));
-
+        Files.write(file.toPath(), OutputType.BYTES.convertFromBase64Png(content));
     }
 }
